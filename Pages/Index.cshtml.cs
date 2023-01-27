42017ab86eae946da67d893cd09dd4ac9944fa6e
jean claude Misigaro <mjeanclaude23@gmail.com>
2023-01-27 17:35:13 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using ZXing;
+using ZXing.Rendering;
+using System.Collections.Generic;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+namespace NetCoreQRCodeChallenge.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://www.hdsr.life", QRCodeGenerator.ECCLevel.Q);
+            QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "qr.png");
+            qrCodeImage.Save(filePath);
+
+        }
+    }
+
+
+
+}
